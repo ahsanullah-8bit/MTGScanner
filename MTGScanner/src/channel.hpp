57d1248203df45a5d8295b6c37825c8106957147
Ahsan Ullah <hatrakhan@gmail.com>
2026-06-10 21:36:21 +0500

    Abstract away outVideoSink in Channel

--- MTGScanner/src/channel.hpp
+++ MTGScanner/src/channel.hpp
@@ -17,31 +17,41 @@ class AbstractChannel : public QObject {
     QML_ELEMENT
     QML_UNCREATABLE("Channel objects are managed by the Engine and this is an abstract class")
 protected:
+    Q_PROPERTY(QString channelType READ channelType CONSTANT)
     Q_PROPERTY(ChannelOptions options READ constOptions WRITE setOptions NOTIFY optionsChanged FINAL)
     Q_PROPERTY(ChannelMetrics* metrics READ metrics WRITE setMetrics NOTIFY metricsChanged FINAL)
+    Q_PROPERTY(QVideoSink* outVideoSink READ outVideoSink WRITE setOutVideoSink NOTIFY outVideoSinkChanged FINAL)
 public:
-    explicit AbstractChannel(QObject *parent = nullptr) : QObject(parent) {}
+    explicit AbstractChannel(const QString &channelType, QObject *parent = nullptr) : QObject(parent), m_channelType(channelType) {}
     virtual ~AbstractChannel() {}
+    QString channelType() const;
     ChannelOptions &options();
     const ChannelOptions &constOptions() const;
     ChannelMetrics *metrics() const;
+    QVideoSink *outVideoSink() const;
 
 public slots:
     void setOptions(const ChannelOptions &options);
     void setMetrics(ChannelMetrics *metrics);
+    void setOutVideoSink(QVideoSink *outVideoSink);
 
 signals:
     void optionsChanged();
     void metricsChanged();
+    void outVideoSinkChanged();
 
 private:
+    QString m_channelType;
     ChannelOptions m_options;
     ChannelMetrics *m_metrics = nullptr;
+    QVideoSink *m_outVideoSink = nullptr;
 };
 
+inline QString AbstractChannel::channelType() const { return m_channelType; }
 inline ChannelOptions &AbstractChannel::options() { return m_options; }
 inline const ChannelOptions &AbstractChannel::constOptions() const { return m_options; }
 inline ChannelMetrics *AbstractChannel::metrics() const { return m_metrics; }
+inline QVideoSink *AbstractChannel::outVideoSink() const { return m_outVideoSink; }
 
 inline void AbstractChannel::setOptions(const ChannelOptions &options) {
     if (m_options == options) return;
@@ -55,6 +65,12 @@ inline void AbstractChannel::setMetrics(ChannelMetrics *metrics) {
     emit metricsChanged();
 }
 
+inline void AbstractChannel::setOutVideoSink(QVideoSink *outVideoSink) {
+    if (m_outVideoSink == outVideoSink) return;
+    m_outVideoSink = outVideoSink;
+    emit outVideoSinkChanged();
+}
+
 // This lives on the Engine (main thread).
 class Channel : public AbstractChannel {
     Q_OBJECT
@@ -62,32 +78,26 @@ class Channel : public AbstractChannel {
     QML_UNCREATABLE("Channel objects are managed by the Engine")
     Q_PROPERTY(QCamera* camera READ camera WRITE setCamera NOTIFY cameraChanged FINAL)
     Q_PROPERTY(QMediaCaptureSession* captureSession READ captureSession WRITE setCaptureSession NOTIFY captureSessionChanged FINAL)
-    Q_PROPERTY(QVideoSink* outVideoSink READ outVideoSink WRITE setOutVideoSink NOTIFY outVideoSinkChanged FINAL)
 public:
-    explicit Channel(QObject *parent = nullptr) : AbstractChannel(parent) {}
+    explicit Channel(QObject *parent = nullptr) : AbstractChannel("Channel", parent) {}
     QCamera *camera() const;
     QMediaCaptureSession *captureSession() const;
-    QVideoSink *outVideoSink() const;
 
 public slots:
     void setCamera(QCamera *camera);
     void setCaptureSession(QMediaCaptureSession *captureSession);
-    void setOutVideoSink(QVideoSink *outVideoSink);
 
 signals:
     void cameraChanged();
     void captureSessionChanged();
-    void outVideoSinkChanged();
 
 private:
     QCamera *m_camera = nullptr;
     QMediaCaptureSession *m_captureSession = nullptr;
-    QVideoSink* m_outVideoSink = nullptr;
 };
 
 inline QCamera* Channel::camera() const { return m_camera; }
 inline QMediaCaptureSession* Channel::captureSession() const { return m_captureSession; }
-inline QVideoSink* Channel::outVideoSink() const { return m_outVideoSink; }
 
 inline void Channel::setCamera(QCamera *camera) {
     if (m_camera == camera) return;
@@ -99,12 +109,6 @@ inline void Channel::setCaptureSession(QMediaCaptureSession *captureSession) {
     if (m_captureSession == captureSession) return;
     m_captureSession = captureSession;
     emit captureSessionChanged();
-}
-
-inline void Channel::setOutVideoSink(QVideoSink *outVideoSink) {
-    if (m_outVideoSink == outVideoSink) return;
-    m_outVideoSink = outVideoSink;
-    emit outVideoSinkChanged();
 }
 
 } // namespace MTGS
